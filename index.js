908b26f7c40e7d906ccf5c91f13d1f6f1134559a
edai <edai@uw.edu>
2022-12-14 21:59:25 -0800

    its my site POG

---
+++ index.js
@@ -0,0 +1,75 @@
+/*
+ * Evelyn Dai
+ * 2-11-2022
+ * AG Wen Qiu
+ *
+ * Javascript file to provide some dynamic changes to index.html elements and UI.
+ */
+
+"use strict";
+(function() {
+  window.addEventListener("load", init);
+
+  /** Initializing function on load */
+  function init() {
+    id("bundleclients").addEventListener("change", showBundle);
+    id("badgebutton").addEventListener("click", spawnBadge);
+  }
+
+  /** Used to show the correct set of images when selected in the dropdown */
+  function showBundle() {
+    let choice = id("bundleclients").value;
+    let qsaArray = qsa(".bundle");
+    for (let i = 0; i < qsaArray.length; i++) {
+      qsaArray[i].classList.add("hidden");
+    }
+    if (choice === "playdohbear") {
+      id('playdohcomm').classList.remove('hidden');
+    } else if (choice === "iamlegend") {
+      id('legendcomm').classList.remove('hidden');
+    } else if (choice === "scabbaz") {
+      id('scabbazcomm').classList.remove('hidden');
+    }
+  }
+
+  /** Spawns a random cat image out of 7 possible images */
+  function spawnBadge() {
+    let newCat = document.createElement('img');
+    let catArray = [];
+    catArray[0] = "img/playdohbear/badge1.png";
+    catArray[1] = "img/playdohbear/badge2.png";
+    catArray[2] = "img/playdohbear/badge3.png";
+    catArray[3] = "img/playdohbear/badge4.png";
+    catArray[4] = "img/playdohbear/badge5.png";
+    catArray[5] = "img/playdohbear/badge6.png";
+    catArray[6] = "img/playdohbear/badge7.png";
+    newCat.src = catArray[Math.floor(Math.random() * catArray.length)];
+    id("spawnedcats").appendChild(newCat);
+  }
+
+  /** Displays an error message if any error during the fetch process occurs */
+  function errorHandler() {
+    let errorMsg = document.createElement("p");
+    errorMsg.textContent = "Sorry, Bored API is not available right now.";
+    id('boredsection').innerHTML = '';
+    id('boredsection').appendChild(errorMsg);
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} id - element ID
+   * @return {object} DOM object associated with id.
+   */
+  function id(id) {
+    return document.getElementById(id);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
+  }
+})();
